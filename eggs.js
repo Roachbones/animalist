f7bc661b67584082fb2c11a64a2537eab46be94b
Vivian Rose <26861054+Roachbones@users.noreply.github.com>
2026-02-13 20:06:43 -0600

    Yes, bobcats are lynxes

--- eggs.js
+++ eggs.js
@@ -334,6 +334,9 @@ function ancestry_egg_message(guess, descendant_id, ancestor_id) {
         }
         return "(Yes, mosquitos are flies.)";
     }
+    if (descendant_id==LOWER_TITLE_TO_ID.bobcat && ancestor_id==LOWER_TITLE_TO_ID.lynx) {
+        return "Yes, bobcats are lynxes. If you want credit for both, name of the other three lynxes.";
+    }
     if (descendant_id==LOWER_TITLE_TO_ID.elk && ancestor_id==LOWER_TITLE_TO_ID.deer) return "(Yes, elk are deer.)";
     if (descendant_id==LOWER_TITLE_TO_ID.moose && ancestor_id==LOWER_TITLE_TO_ID.deer) return "(Yes, moose are deer.)";
     //if (descendant_id=='Q727919' && ancestor_id=='Q83902') {
