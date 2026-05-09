5221c6aabd053361149e189c907e716dfc395faf
Vivian Rose <26861054+Roachbones@users.noreply.github.com>
2026-05-09 16:05:53 -0500

    Draft custom ad metadata

--- premium.js
+++ premium.js
@@ -1,3 +1,10 @@
+CM_URL = 'https://iplayif.com/?story=https%3A%2F%2Fgithub.com%2Fi7%2Fcounterfeit-monkey%2Freleases%2Fdownload%2Fr11.1%2FCounterfeitMonkey-11.gblorb';
+ADS = [
+    '<a href=https://unicornjelly.com title="Unsponsored ad for a comic I like"><img src=ads/alink01.gif alt="Unicorn Jelly HYPER CUTE FUN unicornjelly.com"></a>',
+    '<a href=https://unicornjelly.com title="Unsponsored ad for a comic I like"><img src=ads/unijelly.gif alt="Unicorn Jelly Online Comic MangaStrip Hyper Cute-Cute Super Sweet Fun!"></a>',
+    '<a href='+CM_URL+' title="Unsponsored ad for a game I like"><img src=ads/Atlantida-Squid.gif alt="A squid lounges in the embrace of an Atlantean icon."></a>',
+    '<a href='+CM_URL+' title="Unsponsored ad for a game I like"><img src=ads/MediumCover.png alt="Counterfeit Monkey, a game of word manipulation"></a>'
+]
 
 function tryPremiumCode(code) {
     if (code != code.toUpperCase()) return;
